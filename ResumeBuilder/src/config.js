2d6194100d09e362c79b7aec1aa8771c06cfd1ff
BarathKrishnamoorthy <barathkrishnamoorthy17@gmail.com>
2026-04-29 10:42:58 +0530

    api change

--- ResumeBuilder/src/config.js
+++ ResumeBuilder/src/config.js
@@ -1,6 +1,6 @@
 // export const API_URL = "http://localhost:3015";
 // export const API_URL = "https://api.resumemint.co.uk";
-export const API_URL = "https://airesumebuilder.aryuacademy.com";
+export const API_URL = "https://passats.aryuacademy.com";
 // export const API_URL = "https://portal.aryu.agency";
 // export const API_URL = "http://192.168.0.107:3003";
 
